533cdcf4f617e1b6377fc770103194f47b65f9db
Samuellekpor <samuellekpor@gmail.com>
2022-10-20 21:56:25 +0000

    Add addScore function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const form = document.querySelector('form');
+const refresh = document.querySelector('.refresh-btn');
+const scoresContainer = document.querySelector('.scores-container');
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/`;
+const gameID = 'Pk5uoKBoacOJcYrWGAJB';
+
+const addScore = async (username, score) => {
+  const response = await fetch(`${url}${gameID}/scores/`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ username, score }),
+  });
+  const res = await response.json();
+  return res;
+};
+
