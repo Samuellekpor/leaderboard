611cf23b6bee59da386b0719b4d973084e779c37
Samuellekpor <samuellekpor@gmail.com>
2022-10-21 14:03:41 +0000

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -3,31 +3,8 @@ import './style.css';
 const form = document.querySelector('form');
 const refresh = document.querySelector('.refresh-btn');
 
-const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/`;
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 const gameID = 'JShZvm7cAFqsSmGIRLe0';
-
-const addScore = async (username, score) => {
-  const newScore = {
-    user: username,
-    score: score
-  }
-  const response = await fetch(`${url}${gameID}/scores/`, {
-    method: 'POST',
-    headers: {'Content-type': 'application/json'},
-    body: JSON.stringify(newScore),
-  });
-  const data = await response.json();
-  getScores();
-  return data;
-};
-
-const getScores = async () => {
-  const response = await fetch(`${url}${gameID}/scores/`);
-  const data = await response.json();
-  if (response.ok) {
-    displayScores(data.result);
-  }
-};
 
 const displayScores = (scores) => {
   const scoresContainer = document.querySelector('.scores-container');
@@ -40,11 +17,34 @@ const displayScores = (scores) => {
     <td class="name">${element.score}</td>`;
     scoresContainer.appendChild(row);
   });
-}
+};
+
+const getScores = async () => {
+  const response = await fetch(`${url}${gameID}/scores/`);
+  const data = await response.json();
+  if (response.ok) {
+    displayScores(data.result);
+  }
+};
+
+const addScore = async (username, score) => {
+  const newScore = {
+    user: username,
+    score,
+  };
+  const response = await fetch(`${url}${gameID}/scores/`, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(newScore),
+  });
+  const data = await response.json();
+  getScores();
+  return data;
+};
 
 getScores();
 
-refresh.addEventListener('click', getScores)
+refresh.addEventListener('click', getScores);
 
 form.addEventListener('submit', async (event) => {
   const name = document.querySelector('#name').value;
@@ -53,5 +53,4 @@ form.addEventListener('submit', async (event) => {
   addScore(name, score);
   document.querySelector('#name').value = '';
   document.querySelector('#score').value = '';
-})
-
+});
